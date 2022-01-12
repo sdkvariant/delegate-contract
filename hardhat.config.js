9fba4fac9c9abfb188a162175909c308eee57bc4
chefking <chefking9000@protonmail.com>
2022-01-12 12:20:22 +0100

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('dotenv').config();
+
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
   solidity: "0.7.3",
+  defaultNetwork: 'hardhat',
+  networks: {
+    mainnet: {
+      url: process.env.MAINNET_PROVIDER,
+    },
+    local: {
+      url: 'http://localhost:8545'
+    },
+    hardhat: {
+      forking: {
+        url: process.env.MAINNET_PROVIDER
+      }
+    }
+  },
+  mocha: {
+    timeout: 600000
+  }
 };
