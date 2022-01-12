e9871f5011cdf2fe0180a44fc4ca3ff70fbc21c3
chefking <chefking9000@protonmail.com>
2022-01-12 11:14:58 +0100

    import libraries & interfaces

---
+++ contracts/Delegator.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.6.0 <0.8.0;
+
+import "./interfaces/aave/IDebtToken.sol";
+import "./interfaces/aave/ILendingPool.sol";
+import "./interfaces/aave/IProtocolDataProvider.sol";
+import "./interfaces/common/IERC20.sol";
+import "./libraries/SafeERC20.sol";
